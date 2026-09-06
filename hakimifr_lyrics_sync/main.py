221ad9cdd872c05259db330ce9ef9aae504198fa
Firdaus Hakimi <hakimifr@proton.me>
2026-09-06 22:47:39 +0800

    fix: properly add ',' when disabling AppleMusic provider

--- hakimifr_lyrics_sync/main.py
+++ hakimifr_lyrics_sync/main.py
@@ -84,7 +84,7 @@ if not dev_token and not media_user_token:
     console.print(
         "Disabling Apple Music provider, APPLE_DEV_TOKEN and APPLE_MEDIA_USER_TOKEN is unset"
     )
-    disabled_providers += AppleMusic.id
+    disabled_providers += f",{AppleMusic.id}" if disabled_providers else AppleMusic.id
 
 lyrics_fetcher = LyricsFetcher(
     [
